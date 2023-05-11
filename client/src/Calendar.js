4adabb6c1d7b3f34e1056274ee168f716638d6f0
mariogarzac <mariogarzac99@gmail.com>
2023-05-10 18:30:14 -0600

    Added react calendar

--- client/src/Calendar.js
+++ client/src/Calendar.js
@@ -1,15 +1,34 @@
-import React from "react";
 import "./App.css";
 
-class Calendar extends React.Component {
-	render() {
-		return (
-			<div className="content">
-				<div className="calendar"></div>
-				<h1>Citas</h1>
-			</div>
-		);
-	}
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function MyCalendar() {
+  const [date, setDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setDate(date);
+  };
+
+ const nextWeek = new Date();
+ nextWeek.setDate(nextWeek.getDate() + 7);
+
+  return (
+    <div className="content">
+      <div className="calendar-container">
+       <Calendar className="calendar"
+        value={date}
+        onChange={handleDateChange}
+        calendarType="US"
+        minDate={new Date()}
+        maxDate={nextWeek}
+      />
+      </div>
+    </div>
+  );
 }
 
-export default Calendar;
+export default MyCalendar;
+
+
